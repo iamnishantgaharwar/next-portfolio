f99d64e8c07f70056d24938fb61169b84cfc1e93
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2025-05-23 14:13:06 +0530

    Fix: Mobile View

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
       transition={{ duration: 0.8 }}
       className="min-h-screen flex flex-col overflow-x-hidden"
     >
-      <main className="flex flex-grow">
+      <main className="flex flex-grow flex-col sm:flex-row">
         <nav className="flex m-4">
-          <ul className="space-y-2 border-r-1 min-h-64 pr-4">
+          <ul className="space-y-2 border-b-1 sm:border-b-0 sm:border-r-1 w-full sm:min-h-64 sm:pr-4 flex sm:flex-col justify-around sm:justify-start gap-4 sm:gap-0 py-2 sm:py-0">
             <li className="flex items-center ">
               <span className="cursor-pointer text-base relative inline-block after:absolute after:left-0 after:bottom-0 after:h-1 after:w-0 after:bg-amber-200 after:transition-all after:duration-300 hover:after:w-full">
                 <a href={resume}>resume</a>
@@ -37,17 +37,17 @@ export default function Home() {
             </li>
           </ul>
         </nav>
-        <section className="flex flex-col my-4">
+        <section className="flex flex-col my-4 mx-4 sm:mx-0">
           <h1 className="text-xl font-bold ml-4">Nishant Gaharwar</h1>
           <h2 className="text-xl font-bold ml-4 mt-4">👋 About Me</h2>
           <p className="ml-4 space-y-2 mt-4">
-            <span className="text-base md:w-1/2 sm:w-full block">
+            <span className="text-[1.05rem] md:w-3/4 sm:w-full block leading-relaxed">
               I&apos;m a Software Engineer with 1.5+ years of experience crafting
               scalable, high-performance web applications using TypeScript and
               React. I&apos;m passionate about solving real-world problems, improving
               system efficiency, and contributing to meaningful projects.
             </span>
-            <span className="text-base md:w-1/2 sm:w-full block">
+            <span className="text-[1.05rem] md:w-3/4 sm:w-full block leading-relaxed">
               I&apos;pride myself on being a fast learner who thrives on adapting to
               new technologies and enhancing technical and analytical
               capabilities. I approach development with logic, structure, and a
